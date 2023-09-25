dbc9518b119c4fd061d45b6b750f1860a90999e1
Juan Pablo Sánchez Magariños <juan.sanchez@mikroways.net>
2023-09-25 20:49:13 -0300

    Remove comments, change ip

--- 1/sha512/server-run.sh
+++ 1/sha512/server-run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#gcc -w client.c -lm -o client
-#gcc -w server.c -lm -o server
-gcc -w client.c checksum.c -lm -o client -lssl -lcrypto
 gcc -w server.c checksum.c -lm -o server -lssl -lcrypto
 
 echo -e "10³\t\t10⁴\t\t10⁵\t\t10⁶\t\t10⁷"
@@ -10,7 +7,7 @@ for i in {1..100}; do
 for exp in 3 4 5 6 7; do
     size=$((10**exp))
 
-    ./server 4001 $size
+    ./server 4003 $size
 done
 echo ""
 done
