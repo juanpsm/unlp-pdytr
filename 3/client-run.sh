3bf9890d0cb48493bfc27a6d7648bd19d9e1c340
Intimaria <intimaria@gmail.com>
2023-09-29 00:21:32 -0300

    Reduce experiment number

--- 3/client-run.sh
+++ 3/client-run.sh
@@ -3,10 +3,10 @@
 gcc -w sha512/client.c sha512/checksum.c -lm -o client -lssl -lcrypto
 
 echo -e "10³\t\t10⁴\t\t10⁵\t\t10⁶\t\t10⁷"
-for i in {1..100}; do
+for i in {1..10}; do
 for exp in 3 4 5 6 7; do
     size=$((10**exp))
-    ./client 192.168.56.101 4003 $size
+    ./client 192.168.0.101 4003 $size
 done
 echo ""
 done
--- 3/server-run.sh
+++ 3/server-run.sh
@@ -3,7 +3,7 @@
 gcc -w sha512/server.c sha512/checksum.c -lm -o server -lssl -lcrypto
 
 echo -e "10³\t10⁴\t10⁵\t10⁶\t10⁷"
-for i in {1..100}; do
+for i in {1..10}; do
 for exp in 3 4 5 6 7; do
     size=$((10**exp))
 
