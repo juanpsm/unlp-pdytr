60d12720ce9ad39195927f1fec05b99cb76a412e
Inti Tidball <inti.tidball@mikroways.net>
2023-11-13 20:09:39 -0300

    Agregar archivo y arg

--- tp4/3/3.agente.sh
+++ tp4/3/3.agente.sh
@@ -1,1 +1,1 @@
-java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost -agents mol:AgenteMovil
+java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost -port 1099 -agents "AgenteCliente:myexamples.AgenteCliente($PWD/hola.txt)"
