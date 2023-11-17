258f99347a49efd750b033f7ca915cb4a1e2d6c5
Inti Tidball <inti.tidball@mikroways.net>
2023-11-17 01:27:21 -0300

    Fix first part of readwrite method & parse arguments

--- tp4/3/FTPAgent.java
+++ tp4/3/FTPAgent.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 public class FTPAgent extends Agent {
     private Location currentLocation;
+    private Location originLocation;
+    private Location remoteLocation;
     private String operation;
     private String localPath;
     private String remotePath;
@@ -18,48 +20,49 @@ public class FTPAgent extends Agent {
         Object[] args = getArguments();
         processArguments(args);
         currentLocation = here();
+        originLocation = currentLocation;
         initiateMigration();
     }
 
-  private void processArguments(Object[] args) {
-      if (args.length != 3) {
-          System.out.println("Incorrect number of arguments. Expected format: [operation] [localPath] [remotePath]");
-          doDelete();
-          return;
-      }
+private void processArguments(Object[] args) {
+    if (args.length != 3) {
+        System.out.println("Incorrect number of arguments. Expected format: [operation] [localPath] [remotePath]");
+        doDelete();
+        return;
+    }
 
-      operation = (String) args[0];
-      localPath = (String) args[1];
-      remotePath = (String) args[2];
+    operation = (String) args[0];
+    localPath = (String) args[1];
+    remotePath = (String) args[2];
 
-      try {
-          switch (operation) {
-              case "write":
-                  totalBytes = Files.size(Paths.get(localPath));
-                  buffer = FTPUtils.readFile(localPath, transferredBytes, totalBytes);
-                  break;
-              case "read":
-                  totalBytes = Files.size(Paths.get(remotePath));
-                  break;
-              case "readwrite":
-                  totalBytes = Files.size(Paths.get(remotePath));
-                  buffer = FTPUtils.readFile(remotePath, transferredBytes, totalBytes);
-                  break;
-              default:
-                  System.out.println("Invalid operation: " + operation);
-                  doDelete();
-          }
-      } catch (IOException e) {
-          e.printStackTrace();
-          doDelete();
-      }
-  }
+    try {
+        switch (operation) {
+            case "write":
+                totalBytes = Files.size(Paths.get(localPath));
+                buffer = FTPUtils.readFile(localPath, transferredBytes, totalBytes);
+                break;
+            case "read":
+                totalBytes = Files.size(Paths.get(remotePath));
+                break;
+            case "readwrite":
+                totalBytes = Files.size(Paths.get(localPath));
+                buffer = FTPUtils.readFile(localPath, transferredBytes, totalBytes);
+                break;
+            default:
+                System.out.println("Invalid operation: " + operation);
+                doDelete();
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+        doDelete();
+    }
+}
 
     private void initiateMigration() {
         try {
-            ContainerID destination = new ContainerID("Main-Container", null);
-            System.out.println("Migrating agent to " + destination.getID());
-            doMove(destination);
+            remoteLocation = new ContainerID("Main-Container", null);
+            System.out.println("Migrating agent to " + remoteLocation.getID());
+            doMove(remoteLocation);
         } catch (Exception e) {
             System.out.println("Migration failed: " + e.getMessage());
             doDelete();
@@ -68,6 +71,8 @@ public class FTPAgent extends Agent {
 
     protected void afterMove() {
         Location current = here();
+
+        System.out.println("originLocation: " +originLocation);
         if ("readwrite".equals(operation)) {
             handleReadWrite(current);
         } else if ("write".equals(operation)) {
@@ -78,33 +83,31 @@ public class FTPAgent extends Agent {
     }
 
 private void handleReadWrite(Location current) {
-    if (!current.getName().equals(currentLocation.getName())) {
+    if (current.equals(originLocation)) {
         if (transferredBytes < totalBytes) {
-            FTPUtils.writeFile(remotePath, buffer);
-            System.out.printf("Written %d bytes to remote\n", buffer.length);
             buffer = FTPUtils.readFile(localPath, transferredBytes, totalBytes);
             transferredBytes += buffer.length;
-            doMove(new ContainerID(currentLocation.getName(), null));
-        } 
-        else {
-            buffer = FTPUtils.readFile(remotePath, transferredBytes - (int)totalBytes, totalBytes);
-            transferredBytes += buffer.length;
-            doMove(new ContainerID(currentLocation.getName(), null));
-        }
+            System.out.printf("Read %d bytes from origin\n", buffer.length);
+            doMove(new ContainerID(remoteLocation.getName(), null));
     } else {
+        String newLocalPath = localPath + ".cpy";
+        buffer = FTPUtils.readFile(remotePath, 0, totalBytes); // Read from the beginning of remote file
+        int writtenBytes = FTPUtils.writeFile(newLocalPath, buffer);
+        transferredBytes += writtenBytes;
+        System.out.printf("Written %d bytes to local copy\n", writtenBytes);
         if (transferredBytes < 2 * totalBytes) {
-            String newLocalPath = "copy-of-" + localPath; 
-            FTPUtils.writeFile(newLocalPath, buffer);
-            System.out.printf("Written %d bytes to local copy\n", buffer.length);
-            if (transferredBytes < totalBytes) {
-                doMove(new ContainerID("Main-Container", null));
-            } else {
-                System.out.println("Read-write operation completed successfully.");
-            }
+            doMove(new ContainerID(remoteLocation.getName(), null));
+        } else {
+            System.out.println("Read-write operation completed successfully.");
+            doDelete();
         }
     }
+        } else if (current.equals(remoteLocation)) {
+        FTPUtils.writeFile(remotePath, buffer);
+        System.out.printf("Written %d bytes to remote\n", buffer.length);
+        doMove(new ContainerID(originLocation.getName(), null));
+    }
 }
-
 
 
 private void handleWrite(Location current) {
